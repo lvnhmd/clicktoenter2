a59d3491c7dec2242095d168676e687b1e0d2bfd
lvnhmd <ali.elvin@gmail.com>
2015-12-30 17:26:11 +0000

    create templates from index.html

--- frontend/js/controllers/Home.js
+++ frontend/js/controllers/Home.js
@@ -1,7 +1,9 @@
 module.exports = Ractive.extend({
   template: require('../../tpl/home'),
   components: {
+    header: require('../views/Header'),
     navigation: require('../views/Navigation'),
+    content: require('../views/Content'),
     appfooter: require('../views/Footer')
   },
   onrender: function() {
