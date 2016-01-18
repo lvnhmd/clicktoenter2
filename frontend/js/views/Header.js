8b696a50e9906d0304bd72144ade9da7f455093b
lvnhmd <ali.elvin@gmail.com>
2016-01-18 17:01:13 +0000

    use ractive 0.6.1

--- frontend/js/views/Header.js
+++ frontend/js/views/Header.js
@@ -1,20 +1,9 @@
-// var MyRactive = Ractive.extend({
-// 	template: require('../../tpl/header')
-// });
-
-// module.exports = new MyRactive({
-// 	onconstruct: function() {
-// 		console.log(' user logged : ' + userModel.isLoisgged());
-// 		// this.data.isLogged = !!userModel.isLogged();
-// 		this.set('isLogged', !!userModel.isLogged());
-// 	}
-// });
 // ¯\(ツ)/¯ 
 module.exports = Ractive.extend({
   template: require('../../tpl/header'),
   onconstruct: function() {
-    // this.data.isLogged = !!userModel.isLogged();
-    console.log('Header.js onconstruct user logged : ' + !!userModel.isLogged());
-	userModel.set('isLogged', !!userModel.isLogged());
+    this.data.isLogged = !!userModel.isLogged();
+ //    console.log('Header.js onconstruct user logged : ' + !!userModel.isLogged());
+	// userModel.set('isLogged', !!userModel.isLogged());
   }
 });
