aa35c3fc6d50398644d71bed2591193a5e988d44
lvnhmd <ali.elvin@gmail.com>
2016-01-18 15:26:57 +0000

    set isLogged to user model to avoid Uncaught Error: Using `ractive.data` is no longer supported - you must use the `ractive.get()` API instead

--- frontend/js/views/Header.js
+++ frontend/js/views/Header.js
@@ -1,6 +1,20 @@
+// var MyRactive = Ractive.extend({
+// 	template: require('../../tpl/header')
+// });
+
+// module.exports = new MyRactive({
+// 	onconstruct: function() {
+// 		console.log(' user logged : ' + userModel.isLoisgged());
+// 		// this.data.isLogged = !!userModel.isLogged();
+// 		this.set('isLogged', !!userModel.isLogged());
+// 	}
+// });
+// ¯\(ツ)/¯ 
 module.exports = Ractive.extend({
   template: require('../../tpl/header'),
   onconstruct: function() {
-    this.data.isLogged = !!userModel.isLogged();
+    // this.data.isLogged = !!userModel.isLogged();
+    console.log('Header.js onconstruct user logged : ' + !!userModel.isLogged());
+	userModel.set('isLogged', !!userModel.isLogged());
   }
 });
