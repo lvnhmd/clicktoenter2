33994df28d71f72e831011222ac5e4948e87cd3d
lvnhmd <ali.elvin@gmail.com>
2016-01-19 15:45:01 +0000

    Managing a user's profile

--- backend/API.js
+++ backend/API.js
@@ -24,7 +24,7 @@ var getDatabaseConnection = function(callback) {
   } else {
     MongoClient.connect('mongodb://127.0.0.1:27017/clicktoenter', function(err, db) {
       if (err) {
-         throw err;
+        throw err;
       };
       database = db;
       callback(database);
@@ -50,99 +50,130 @@ var validEmail = function(value) {
 var Router = require('../frontend/js/lib/router')();
 Router
   .add('api/version', function(req, res) {
-    console.log('api/version'); 
+    console.log('api/version');
     response({
       version: '0.1'
     }, res);
   })
   .add('api/user/login', function(req, res) {
-      processPOSTRequest(req, function(data) {
-        if (!data.email || data.email === '' || !validEmail(data.email)) {
-          error('Invalid or missing email.', res);
-        } else if (!data.password || data.password === '') {
-          error('Please fill your password.', res);
-        } else {
-          getDatabaseConnection(function(db) {
-            var collection = db.collection('user');
-            collection.find({
-              email: data.email,
-              password: sha1(data.password)
-            }).toArray(function(err, result) {
-              if (result.length === 0) {
-                error('Wrong email or password', res);
-              } else {
-                var user = result[0];
-                delete user.password;
-                delete user._id;
-                console.log('req ', req);
-                console.log('req.session ', req.session);
-                req.session.user = user;
-                response({
-                  success: 'OK',
-                  user: user
-                }, res);
-              }
-            });
-          });
-        }
-      });
-    })
-    .add('api/user/logout', function(req, res) {
-      delete req.session.user;
-      response({
-        success: 'OK'
-      }, res);
-    })
-  .add('api/user', function(req, res) {
-      
-      switch (req.method) {
-        case 'GET':
-          if (req.session && req.session.user) {
-            response(req.session.user, res);
-          } else {
-            response({}, res);
-          }
-          break;
-        case 'PUT':
-          // ...
-          break;
-        case 'POST':
-          processPOSTRequest(req, function(data) {
-            console.log('api/user POST');
-            if (!data.firstName || data.firstName === '') {
-              error('Please fill your first name.', res);
-            } else if (!data.lastName || data.lastName === '') {
-              error('Please fill your last name.', res);
-            } else if (!data.email || data.email === '' || !validEmail(data.email)) {
-              error('Invalid or missing email.', res);
-            } else if (!data.password || data.password === '') {
-              error('Please fill your password.', res);
+    processPOSTRequest(req, function(data) {
+      if (!data.email || data.email === '' || !validEmail(data.email)) {
+        error('Invalid or missing email.', res);
+      } else if (!data.password || data.password === '') {
+        error('Please fill your password.', res);
+      } else {
+        getDatabaseConnection(function(db) {
+          var collection = db.collection('user');
+          collection.find({
+            email: data.email,
+            password: sha1(data.password)
+          }).toArray(function(err, result) {
+            if (result.length === 0) {
+              error('Wrong email or password', res);
             } else {
-              getDatabaseConnection(function(db) {
-                var collection = db.collection('user');
-                data.password = sha1(data.password);
-                collection.insert(data, function(err, docs) {
-                  console.log('insert ' + JSON.stringify(data));
-                  response({
-                    success: 'OK'
-                  }, res);
-                });
-              });
+              var user = result[0];
+              delete user.password;
+              delete user._id;
+              console.log('req ', req);
+              console.log('req.session ', req.session);
+              req.session.user = user;
+              response({
+                success: 'OK',
+                user: user
+              }, res);
             }
           });
-          break;
-        case 'DELETE':
-          // ...
-          break;
-      };
+        });
+      }
+    });
+  })
+  .add('api/user/logout', function(req, res) {
+    delete req.session.user;
+    response({
+      success: 'OK'
+    }, res);
+  })
+  .add('api/user', function(req, res) {
+
+    switch (req.method) {
+      case 'GET':
+        if (req.session && req.session.user) {
+          response(req.session.user, res);
+        } else {
+          response({}, res);
+        }
+        break;
+      case 'PUT':
+        processPOSTRequest(req, function(data) {
+          if (!data.firstName || data.firstName === '') {
+            error('Please fill your first name.', res);
+          } else if (!data.lastName || data.lastName === '') {
+            error('Please fill your last name.', res);
+          } else {
+            getDatabaseConnection(function(db) {
+              var collection = db.collection('user');
+              if (data.password) {
+                data.password = sha1(data.password);
+              }
+              collection.update({
+                  email: req.session.user.email
+                }, {
+                  $set: data
+                },
+                function(err, result) {
+                  if (err) {
+                    err('Error updating the data.');
+                  } else {
+                    if (data.password) delete data.password;
+                    for (var key in data) {
+                      req.session.user[key] = data[key];
+                    }
+                    response({
+                      success: 'OK'
+                    }, res);
+                  }
+                }
+              );
+            });
+          }
+        });
+        break;
+      case 'POST':
+        processPOSTRequest(req, function(data) {
+          console.log('api/user POST');
+          if (!data.firstName || data.firstName === '') {
+            error('Please fill your first name.', res);
+          } else if (!data.lastName || data.lastName === '') {
+            error('Please fill your last name.', res);
+          } else if (!data.email || data.email === '' || !validEmail(data.email)) {
+            error('Invalid or missing email.', res);
+          } else if (!data.password || data.password === '') {
+            error('Please fill your password.', res);
+          } else {
+            getDatabaseConnection(function(db) {
+              var collection = db.collection('user');
+              data.password = sha1(data.password);
+              collection.insert(data, function(err, docs) {
+                console.log('insert ' + JSON.stringify(data));
+                response({
+                  success: 'OK'
+                }, res);
+              });
+            });
+          }
+        });
+        break;
+      case 'DELETE':
+        // ...
+        break;
+    };
   })
   .add(function(req, res) {
-    console.log('default'); 
+    console.log('default');
     response({
       success: true
     }, res);
-  })
-  ;
+  });
 
 module.exports = function(req, res) {
   Router.check(req.url, [req, res]);
